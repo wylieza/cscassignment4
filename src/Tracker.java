31ce0e9a126267430626306c303dde8eebe199de
SpectrumX <wylieza@gmail.com>
2019-09-21 12:45:49 +0200

    Created a tracker class to monitor global variables and trigger updates on changes

---
+++ src/Tracker.java
@@ -0,0 +1,43 @@
+public class Tracker extends Thread{
+
+    private int lastMissedWords;
+	private int lastCaughtWords;
+	private int lastGameScore;
+
+    WordPanel w;
+    Score s;
+
+    public Tracker(WordPanel w, Score s){
+        this.w = w;
+        this.s = s;
+    }
+
+    public void run(){
+        while(WordApp.live){
+            if(lastMissedWords < s.getMissed()){
+                lastMissedWords = s.getMissed(); //This must happend before the update and then the update must happen on the local variable so we don't 'miss' a score
+                WordApp.updateMissed("Missed:" + lastMissedWords + "    ");
+                System.out.println("Missed Word!");
+                w.repaint();
+            }
+            if(lastCaughtWords < s.getCaught()){
+                lastCaughtWords = s.getCaught();
+                WordApp.updateCaught("Caught: " + lastCaughtWords + "    ");
+                System.out.println(lastCaughtWords);
+                w.repaint();
+            }
+            if(lastGameScore < s.getScore()){
+                lastGameScore = s.getScore();
+                WordApp.updateScore("Score:" + lastGameScore + "    "); 
+                System.out.println("Current Score " + lastGameScore);
+                w.repaint();
+            }
+        }
+        //End of game procedure...?
+    }
+    
+}
+
+
+		
+		
